067741197889223297901746652683d5715219a0
Ariana Lee <147213703+ChardineL@users.noreply.github.com>
2025-09-16 13:48:37 -0400

    Added MC/Player GameObject
    i created a GameObject for the player, which includes the idle, walk, run, dash, and death animations (run, dash, and death haven't been implemented yet). Also created a PlayerController.cs script that allows you to move the player.
    
    Stuff that still needs to be done:
    - fix disappearing sprite (sprite only appears if the player is moving; doesn't show idle animation)
    - implement the run, dash, and death animations by updating the PlayerController.cs script
    - add collisions between the player and the tilemap (walls)

---
+++ CAP4053/Assets/Scripts/MC_Player Movement/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // get input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // flip sprite (left/right)
+        if (movement.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1); // face right
+        else if (movement.x < 0)
+            transform.localScale = new Vector3(1, 1, 1); // face left
+            
+        // set animator parameter
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        // move player
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
