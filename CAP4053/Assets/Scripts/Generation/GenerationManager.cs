16eeeb7c348522b30fe41b8aa6d86fbc1109596f
Codster177 <codster157@gmail.com>
2025-09-23 00:55:48 -0400

    WIP Room Generation

---
+++ CAP4053/Assets/Scripts/Generation/GenerationManager.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GenerationManager : MonoBehaviour
+{
+    public static GenerationManager publicGenerationManager;
+    [SerializeField] private GameObject roomPrefabs;
+
+}
---
+++ CAP4053/Assets/Scripts/Generation/RoomPrefab.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RoomPrefab : MonoBehaviour
+{
+    public Grid roomGrid;
+    public Tilemap floormap;
+    public Tilemap wallmap;
+
+}
