53fa69557e89c91505f79421c95ee77d71ddd365
Codster177 <codster157@gmail.com>
2025-09-28 00:25:01 -0400

    Room Generation WIP - Graph Edges are set up

--- CAP4053/Assets/Scripts/Generation/GenerationManager.cs
+++ CAP4053/Assets/Scripts/Generation/GenerationManager.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Splines.ExtrusionShapes;
 using UnityEngine.Tilemaps;
@@ -25,17 +27,17 @@ public class GenerationManager : MonoBehaviour
 
     void Start()
     {
-        GenerateSegment(new Vector2(0f, 0f), squareBounds, minimumRoomRange, testGenerationDirection);
+        GenerateSegment(new Vector2(0f, 0f), minimumRoomRange, testGenerationDirection);
     }
 
     // Segment is a square grid going from (0,0) to (squareBounds, squareBounds).
     // First, determing the connecting room's grid position. For example, if the direction's up, it might be (0,2).
     // When generating, loop from (0,0) to (squareBounds, squareBounds), and to find the room's vector location, use
     // the CalculateVectorPos function to find the room's vector position.  
-    public void GenerateSegment(Vector2 startingPosition, int squareBounds, int[] minimumRoomRange, GenerationDirection direction)
+    public void GenerateSegment(Vector2 startingPosition, int[] minimumRoomRange, GenerationDirection direction)
     {
         // Finds the position of the connecting room to the new segment.
-        int[] connectingGridInfo = DetermineLocalOffset(direction, squareBounds);
+        int[] connectingGridInfo = DetermineLocalOffset(direction);
 
         // Calculates the offset that the position is multiplied by.
         int[] localOffset = new int[2] { 0 - connectingGridInfo[0], 0 - connectingGridInfo[1] };
@@ -70,37 +72,35 @@ public class GenerationManager : MonoBehaviour
 
         for (int i = 0; i < spawnedIndexList.Count; i++)
         {
-            int[] gridCoords = ConvertGridIndex(spawnedIndexList[i], squareBounds);
+            int[] gridCoords = ConvertGridIndex(spawnedIndexList[i]);
 
             Vector2 roomVectorPos = CalculateVectorPos(startingPosition, localOffset, gridCoords);
             RoomPrefab newRoom = GenerateRoom(roomVectorPos, roomPrefabs[1]);
             currentlyGenerating.Add(newRoom);
         }
-    }
-    // private List<int> ConnectSegment(List<int> spawnedRoomIndices, List<int> notSpawnedIndices)
-    // {
 
-    // }
+        ConnectSegment(spawnedIndexList, roomIndexList);
+    }
     private RoomPrefab GenerateRoom(Vector2 position, RoomPrefab prefab)
     {
         GameObject newRoom = Instantiate(prefab.roomPrefab, position, Quaternion.identity);
         return newRoom.GetComponent<RoomPrefab>();
     }
 
-    private int[] ConvertGridIndex(int index, int squareBounds)
+    private int[] ConvertGridIndex(int index)
     {
         int[] gridCoords = new int[2];
         gridCoords[0] = index / squareBounds;
         gridCoords[1] = index % squareBounds;
         return gridCoords;
     }
-    private int ConvertGridCoords(int[] cords, int squareBounds)
+    private int ConvertGridCoords(int[] cords)
     {
         int index = 0;
         index = (cords[0] * squareBounds) + cords[1];
         return index;
     }
-    private int[] DetermineLocalOffset(GenerationDirection direction, int squareBounds)
+    private int[] DetermineLocalOffset(GenerationDirection direction)
     {
         int[] connectingGridPos = new int[3];
         int xGrid, yGrid, indexVal;
@@ -138,11 +138,117 @@ public class GenerationManager : MonoBehaviour
         return new Vector2(startingPosition.x + (xOffsetConst * (gridPos[0] + localOffset[0])),
         startingPosition.y + (yOffsetConst * (gridPos[1] + localOffset[1])));
     }
+    private void ConnectSegment(List<int> spawnedRoomIndices, List<int> notSpawnedIndices)
+    {
+        List<SpawnNode> nodeList = new List<SpawnNode>();
 
+        for (int i = 0; i < spawnedRoomIndices.Count; i++)
+        {
+            nodeList.Add(new SpawnNode(spawnedRoomIndices[i], true));
+        }
+        for (int i = 0; i < notSpawnedIndices.Count; i++)
+        {
+            nodeList.Add(new SpawnNode(notSpawnedIndices[i], false));
+        }
+        nodeList.Sort();
+        // for (int i = 0; i < nodeList.Count; i++)
+        // {
+        //     Debug.Log("nodeList[" + i + "]: " + nodeList[i].spawnIndex);
+        // }
+        List<Edge> edgeList = MakeEdges(nodeList);
+        for (int i = 0; i < edgeList.Count; i++)
+        {
+            int[] node1Coords = ConvertGridIndex(edgeList[i].node1.spawnIndex);
+            int[] node2Coords = ConvertGridIndex(edgeList[i].node2.spawnIndex);
+            Debug.Log("edgeList[" + i + "]: Node 1 = " + node1Coords[0] + " " + node1Coords[1] + ", Node 2 = " + node2Coords[0] + " " + node2Coords[1] + ", Weight = " + edgeList[i].weight);
+        }
+    }
+    private List<Edge> MakeEdges(List<SpawnNode> nodeList)
+    {
+        List<Edge> edgeList = new List<Edge>();
+        for (int i = 0; i < nodeList.Count; i++)
+        {
+            int[] gridCoords = ConvertGridIndex(nodeList[i].spawnIndex);
+            SpawnNode newNode;
+            if (gridCoords[0] != (squareBounds - 1))
+            {
+                newNode = nodeList[ConvertGridCoords(new int[] { gridCoords[0] + 1, gridCoords[1] })];
+
+                int weight = -1;
+                if (nodeList[i].Activated && newNode.Activated)
+                {
+                    weight = 1;
+                }
+                else if (nodeList[i].Activated || newNode.Activated)
+                {
+                    weight = 2;
+                }
+                else
+                {
+                    weight = 3;
+                }
+                edgeList.Add(new Edge(nodeList[i], newNode, weight));
+            }
+            if (gridCoords[1] != (squareBounds - 1))
+            {
+                newNode = nodeList[ConvertGridCoords(new int[] { gridCoords[0], gridCoords[1] + 1 })];
+
+                int weight = -1;
+                if (nodeList[i].Activated && newNode.Activated)
+                {
+                    weight = 1;
+                }
+                else if (nodeList[i].Activated || newNode.Activated)
+                {
+                    weight = 2;
+                }
+                else
+                {
+                    weight = 3;
+                }
+                edgeList.Add(new Edge(nodeList[i], newNode, weight));
+            }
+        }
+        return edgeList;
+    }
+
+    private class SpawnNode : IComparable
+    {
+        public int spawnIndex;
+        public bool Activated;
+        public SpawnNode(int index, bool setActivated)
+        {
+            spawnIndex = index;
+            Activated = setActivated;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+            SpawnNode otherNode = obj as SpawnNode;
+            if (otherNode != null)
+            {
+                return this.spawnIndex.CompareTo(otherNode.spawnIndex);
+            }
+            else
+            {
+                throw new ArgumentException("GenerationManager: SpawnNode: Comparing object to something that is not a SpawnNode.");
+            }
+        }
+    }
     private class Edge
     {
-        public int[] indices = new int[2];
+        public SpawnNode node1, node2;
         public int weight;
+        public Edge(SpawnNode newNode1, SpawnNode newNode2, int newWeight)
+        {
+            node1 = newNode1;
+            node2 = newNode2;
+            weight = newWeight;
+        }
     }
 }
 
