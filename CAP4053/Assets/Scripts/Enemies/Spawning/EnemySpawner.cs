eb58ad8df64e4b6308e560cf7f7062f81a90b306
Codster177 <codster157@gmail.com>
2025-10-09 14:41:28 -0400

    push

--- CAP4053/Assets/Scripts/Enemies/Spawning/EnemySpawner.cs
+++ CAP4053/Assets/Scripts/Enemies/Spawning/EnemySpawner.cs
@@ -4,9 +4,10 @@ using UnityEngine.UIElements;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D boxCollider;
-    float xBound, yBound;
+    float xMin, xMax, yMin, yMax;
     void Start()
     {
         Vector3 center = boxCollider.transform.position + new Vector3(boxCollider.offset.x, boxCollider.offset.y, 0f);
+
     }
 }
